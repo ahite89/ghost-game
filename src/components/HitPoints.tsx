1144bca7f8ce62011f0424e539c8807d37faca1d
Alexander Hite <alexander.hite@healthez.com>
2025-04-09 15:45:33 -0500

    add gh pages deployment code

--- src/components/HitPoints.tsx
+++ src/components/HitPoints.tsx
@@ -7,6 +7,24 @@ import { FULL_HP_ARRAY } from "../constants/hitPoints";
 
 export default function HitPoints({currentHP}: HitPointsProps) {
 
+//     <AnimatePresence mode="popLayout">
+//     {isFull ? (
+//       <motion.span
+//         key="full"
+//         initial={{ scale: 1 }}
+//         animate={{ scale: 1.2 }}
+//         exit={{ scale: 2, opacity: 0 }}
+//         transition={{ duration: 0.2, ease: "easeOut" }}
+//       >
+//         <FaHeart color="red" />
+//       </motion.span>
+//     ) : (
+//       <motion.span key="empty" initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.2 }}>
+//         <FaRegHeart color="gray" />
+//       </motion.span>
+//     )}
+//   </AnimatePresence>
+
     const renderedHitPoints = FULL_HP_ARRAY.map(hitPoint => {
         return (
             <motion.div
